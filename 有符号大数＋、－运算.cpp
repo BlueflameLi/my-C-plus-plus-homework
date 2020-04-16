737f85f338dc8c1c745cf216d2aa9259f564e13a
lizhihao <310645359@qq.com>
2020-04-16 21:40:49 +0800

    Update 有符号大数＋、－运算.cpp
    改成将无符号类直接作为数据成员

--- 有符号大数＋、－运算.cpp
+++ 有符号大数＋、－运算.cpp
@@ -2,50 +2,54 @@
 #include <string>
 #include <algorithm>
 using namespace std;
-class signed_Bignumber
+class signed_Bignumber;
+class Unsigned_Bignumber
 {
 private:
 	string number;
-	bool sgn;
+	friend class signed_Bignumber;
 
 public:
-	signed_Bignumber operator+(const signed_Bignumber &rhs) const; //加法重载
-	signed_Bignumber operator-(const signed_Bignumber &rhs) const; //减法重载
+	Unsigned_Bignumber operator+(const Unsigned_Bignumber &rhs) const; //加法重载
+	Unsigned_Bignumber operator-(const Unsigned_Bignumber &rhs) const; //减法重载
 
-	friend istream &operator>>(istream &is, signed_Bignumber &rhs);		  //输入重载
-	friend ostream &operator<<(ostream &os, const signed_Bignumber &rhs); //输出重载
+	friend istream &operator>>(istream &is, Unsigned_Bignumber &rhs); //输入重载
+	friend istream &operator>>(istream &is, signed_Bignumber &rhs);
+	friend ostream &operator<<(ostream &os, const Unsigned_Bignumber &rhs); //输出重载
+	friend ostream &operator<<(ostream &os, const signed_Bignumber &rhs);
 };
-string add(const string &s1, const string &s2) //无符号高精度加法
+
+Unsigned_Bignumber Unsigned_Bignumber::operator+(const Unsigned_Bignumber &rhs) const
 {
-	int l1 = s1.length();
-	int l2 = s2.length();
-	string str;
+	int l1 = number.length();
+	int l2 = rhs.number.length();
+	Unsigned_Bignumber C;
 	int t = 0; //进位
 	for (int i = 0, j = 0; i < l1 || j < l2 || t; i++, j++)
 	{
 		if (i < l1)
-			t += s1[i] - '0';
+			t += number[i] - '0';
 		if (j < l2)
-			t += s2[i] - '0';
-		str += t % 10 + '0';
+			t += rhs.number[i] - '0';
+		C.number += t % 10 + '0';
 		t /= 10;
 	}
-	return str;
+	return C;
 }
-string sub(const string &s1, const string &s2) //无符号高精度减法
+Unsigned_Bignumber Unsigned_Bignumber::operator-(const Unsigned_Bignumber &rhs) const
 {
-	string str;
-	if (s1 == s2)
-		str = "0";
+	Unsigned_Bignumber C;
+	if (number == rhs.number)
+		C.number = "0";
 	else
 	{
 		int t = 0;
 		int tmp = 0; //退位
-		int l1 = s1.length();
-		int l2 = s2.length();
+		int l1 = number.length();
+		int l2 = rhs.number.length();
 		for (int i = 0; i < l2; i++)
 		{
-			t = (s1[i] - '0') - (s2[i] - '0') - tmp;
+			t = (number[i] - '0') - (rhs.number[i] - '0') - tmp;
 			if (t < 0)
 			{
 				tmp = 1;
@@ -53,11 +57,11 @@ string sub(const string &s1, const string &s2) //无符号高精度减法
 			}
 			else
 				tmp = 0;
-			str += t + '0';
+			C.number += t + '0';
 		}
 		for (int i = l2; i < l1; i++)
 		{
-			t = s1[i] - '0' - tmp;
+			t = number[i] - '0' - tmp;
 			if (t < 0)
 			{
 				tmp = 1;
@@ -66,96 +70,20 @@ string sub(const string &s1, const string &s2) //无符号高精度减法
 			else
 				tmp = 0;
 			if (i < l1 - 1 || t)
-				str += t + '0';
+				C.number += t + '0';
 		}
-	}
-	return str;
-}
-signed_Bignumber signed_Bignumber::operator+(const signed_Bignumber &rhs) const
-{
-	signed_Bignumber C;
-	if (sgn && rhs.sgn) //(-a)+(-b)=-(a+b)
-	{
-		C.number = add(number, rhs.number);
-		C.sgn = true;
-	}
-	else if (sgn && !rhs.sgn)																						  //(-a)+b=b-a
-		if (number.length() > rhs.number.length() || (number.length() == rhs.number.length() && number > rhs.number)) //若a>b，b-a=-(a-b)
-		{
-			C.number = sub(number, rhs.number);
-			C.sgn = true;
-		}
-		else
-		{
-			C.number = sub(rhs.number, number);
-			C.sgn = false;
-		}
-	else if (!sgn && rhs.sgn)																						  //a+(-b)=a-b
-		if (number.length() < rhs.number.length() || (number.length() == rhs.number.length() && number < rhs.number)) //若b>a,a-b=-(b-a)
-		{
-			C.number = sub(rhs.number, number);
-			C.sgn = true;
-		}
-		else
-		{
-			C.number = sub(number, rhs.number);
-			C.sgn = false;
-		}
-	else
-	{
-		C.number = add(number, rhs.number);
-		C.sgn = false;
-	}
-	return C;
-}
-signed_Bignumber signed_Bignumber::operator-(const signed_Bignumber &rhs) const
-{
-	signed_Bignumber C;
-	if (sgn && rhs.sgn)																								  //(-a)-(-b)=b-a
-		if (number.length() > rhs.number.length() || (number.length() == rhs.number.length() && number > rhs.number)) //若a>b，b-a=-(a-b)
-		{
-			C.number = sub(number, rhs.number);
-			C.sgn = true;
-		}
-		else
-		{
-			C.number = sub(rhs.number, number);
-			C.sgn = false;
-		}
-	else if (sgn && !rhs.sgn) //(-a)-b=-(a+b)
-	{
-		C.number = add(number, rhs.number);
-		C.sgn = true;
-	}
-	else if (!sgn && rhs.sgn) //a-(-b)=a+b
-	{
-		C.number = add(number, rhs.number);
-		C.sgn = false;
-	}
-	else if (number.length() < rhs.number.length() || (number.length() == rhs.number.length() && number < rhs.number)) //若b>a,a-b=-(b-a)
-	{
-		C.number = sub(rhs.number, number);
-		C.sgn = true;
-	}
-	else
-	{
-		C.number = sub(number, rhs.number);
-		C.sgn = false;
 	}
 	return C;
 }
 
-istream &operator>>(istream &is, signed_Bignumber &rhs)
+istream &operator>>(istream &is, Unsigned_Bignumber &rhs)
 {
 	is >> rhs.number;
-	rhs.sgn = false;
 	int i = 0;
 	int l = rhs.number.length();
 	for (i = 0; i < l; i++)
 		if (rhs.number[i] > '0' && rhs.number[i] <= '9')
 			break;
-		else if (rhs.number[i] == '-')
-			rhs.sgn = true;
 	if (i == l)
 		rhs.number = "0";
 	else
@@ -165,10 +93,124 @@ istream &operator>>(istream &is, signed_Bignumber &rhs)
 	}
 	return is;
 }
-ostream &operator<<(ostream &os, const signed_Bignumber &rhs)
+ostream &operator<<(ostream &os, const Unsigned_Bignumber &rhs)
 {
 	string str;
 	str.assign(rhs.number.rbegin(), rhs.number.rend());
+	os << str;
+	return os;
+}
+class signed_Bignumber
+{
+private:
+	Unsigned_Bignumber absolute_value;
+	bool sgn;
+
+public:
+	signed_Bignumber operator+(const signed_Bignumber &rhs) const; //加法重载
+	signed_Bignumber operator-(const signed_Bignumber &rhs) const; //减法重载
+
+	friend istream &operator>>(istream &is, signed_Bignumber &rhs);		  //输入重载
+	friend ostream &operator<<(ostream &os, const signed_Bignumber &rhs); //输出重载
+};
+signed_Bignumber signed_Bignumber::operator+(const signed_Bignumber &rhs) const
+{
+	signed_Bignumber C;
+	if (sgn && rhs.sgn) //(-a)+(-b)=-(a+b)
+	{
+		C.absolute_value = absolute_value + rhs.absolute_value;
+		C.sgn = true;
+	}
+	else if (sgn && !rhs.sgn)																																													//(-a)+b=b-a
+		if (absolute_value.number.length() > rhs.absolute_value.number.length() || (absolute_value.number.length() == rhs.absolute_value.number.length() && absolute_value.number > rhs.absolute_value.number)) //若a>b，b-a=-(a-b)
+		{
+			C.absolute_value = absolute_value - rhs.absolute_value;
+			C.sgn = true;
+		}
+		else
+		{
+			C.absolute_value = rhs.absolute_value - absolute_value;
+			C.sgn = false;
+		}
+	else if (!sgn && rhs.sgn)																																													//a+(-b)=a-b
+		if (absolute_value.number.length() < rhs.absolute_value.number.length() || (absolute_value.number.length() == rhs.absolute_value.number.length() && absolute_value.number < rhs.absolute_value.number)) //若b>a,a-b=-(b-a)
+		{
+			C.absolute_value = rhs.absolute_value - absolute_value;
+			C.sgn = true;
+		}
+		else
+		{
+			C.absolute_value = absolute_value - rhs.absolute_value;
+			C.sgn = false;
+		}
+	else
+	{
+		C.absolute_value = absolute_value + rhs.absolute_value;
+		C.sgn = false;
+	}
+	return C;
+}
+signed_Bignumber signed_Bignumber::operator-(const signed_Bignumber &rhs) const
+{
+	signed_Bignumber C;
+	if (sgn && rhs.sgn)																																															//(-a)-(-b)=b-a
+		if (absolute_value.number.length() > rhs.absolute_value.number.length() || (absolute_value.number.length() == rhs.absolute_value.number.length() && absolute_value.number > rhs.absolute_value.number)) //若a>b，b-a=-(a-b)
+		{
+			C.absolute_value = absolute_value - rhs.absolute_value;
+			C.sgn = true;
+		}
+		else
+		{
+			C.absolute_value = rhs.absolute_value - absolute_value;
+			C.sgn = false;
+		}
+	else if (sgn && !rhs.sgn) //(-a)-b=-(a+b)
+	{
+		C.absolute_value = absolute_value + rhs.absolute_value;
+		C.sgn = true;
+	}
+	else if (!sgn && rhs.sgn) //a-(-b)=a+b
+	{
+		C.absolute_value = absolute_value + rhs.absolute_value;
+		C.sgn = false;
+	}
+	else if (absolute_value.number.length() < rhs.absolute_value.number.length() || (absolute_value.number.length() == rhs.absolute_value.number.length() && absolute_value.number < rhs.absolute_value.number)) //若b>a,a-b=-(b-a)
+	{
+		C.absolute_value = rhs.absolute_value - absolute_value;
+		C.sgn = true;
+	}
+	else
+	{
+		C.absolute_value = absolute_value - rhs.absolute_value;
+		C.sgn = false;
+	}
+	return C;
+}
+
+istream &operator>>(istream &is, signed_Bignumber &rhs)
+{
+	is >> rhs.absolute_value.number;
+	rhs.sgn = false;
+	int i = 0;
+	int l = rhs.absolute_value.number.length();
+	for (i = 0; i < l; i++)
+		if (rhs.absolute_value.number[i] > '0' && rhs.absolute_value.number[i] <= '9')
+			break;
+		else if (rhs.absolute_value.number[i] == '-')
+			rhs.sgn = true;
+	if (i == l)
+		rhs.absolute_value.number = "0";
+	else
+	{
+		rhs.absolute_value.number.erase(0, i);
+		reverse(rhs.absolute_value.number.begin(), rhs.absolute_value.number.end());
+	}
+	return is;
+}
+ostream &operator<<(ostream &os, const signed_Bignumber &rhs)
+{
+	string str;
+	str.assign(rhs.absolute_value.number.rbegin(), rhs.absolute_value.number.rend());
 	if (rhs.sgn)
 		os << '-';
 	os << str;
