9b49b04fca7a72b2f76668c6a51c135f39f3b13c
lizhihao <310645359@qq.com>
2020-04-25 21:37:39 +0800

    Update 虚函数和控件.cpp
    增加了扫尾部分，防止内存泄露

--- 虚函数和控件.cpp
+++ 虚函数和控件.cpp
@@ -6,56 +6,57 @@ using namespace std;
 class CUIControl
 {
 protected:
-    const string label;
+	const string label;
 
 public:
-    CUIControl(string label) : label(label)
-    {
-    }
-    virtual void Clicked() const = 0; //定义为纯虚函数
+	CUIControl(string label) : label(label) {}
+	virtual void Clicked() const = 0; //定义为纯虚函数
+	virtual ~CUIControl() {}
 };
 
 class CMenu : public CUIControl
 {
 public:
-    CMenu(string label) : CUIControl(label)
-    {
-    }
-    void Clicked() const
-    {
-        cout << "menu-" << label << " Clicked" << endl;
-    }
+	CMenu(string label) : CUIControl(label) {}
+	void Clicked() const
+	{
+		cout << "menu-" << label << " Clicked" << endl;
+	}
 };
 
 class CButton : public CUIControl
 {
 public:
-    CButton(string label) : CUIControl(label)
-    {
-    }
-    void Clicked() const
-    {
-        cout << "button-" << label << " Clicked" << endl;
-    }
+	CButton(string label) : CUIControl(label) {}
+	void Clicked() const
+	{
+		cout << "button-" << label << " Clicked" << endl;
+	}
 };
 int main()
 {
-    int n;
-    string type, label;
-    vector<CUIControl *> v;
+	int n;
+	string type, label;
+	vector<CUIControl *> v;
 
-    cin >> n;
-    while (n--)
-    {
-        cin >> type >> label;
-        if (type == "menu")
-            v.push_back(new CMenu(label));
-        else
-            v.push_back(new CButton(label));
-    }
+	cin >> n;
+	while (n--)
+	{
+		cin >> type >> label;
+		if (type == "menu")
+			v.push_back(new CMenu(label));
+		else
+			v.push_back(new CButton(label));
+	}
 
-    int p;
-    while (cin >> type >> p)
-        v[p - 1]->Clicked();
-    return 0;
+	int p;
+	while (cin >> type >> p)
+		v[p - 1]->Clicked();
+	for (auto itr = v.begin(); itr != v.end(); itr++)
+		if (*itr != nullptr)
+		{
+			delete *itr;
+			*itr = nullptr;
+		}
+	return 0;
 }
